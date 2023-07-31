ae658ec624a96dd7606980bd5a3f5d611f00b430
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-07-30 21:19:13 -0700

    refactor(tauri) remove greet function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,13 +4,6 @@
 // App Entry Point; first function called when app is launched
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-// TODO: Remove this example function
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
 }
