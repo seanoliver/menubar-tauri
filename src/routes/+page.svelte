61cf2a5e8f9dc52f69918824d860fd21a7fd4002
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-07-30 21:21:00 -0700

    feat(page.svelte) add iframe functionality and window resize event, remove greet component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,32 @@
 <script>
-	import Greet from '../lib/greet.svelte';
+    let width = 0;
+    let height = 0;
+	let frameWidth = 0;
+	let numFrames = 0;
+	let frames = [];
+
+	const inlineFrames = ['https://localhost:5173/openai/', 'http://localhost:5173/claude2'];
+
+	numFrames = inlineFrames.length;
+
+    if (typeof window !== 'undefined') {
+        width = window.innerWidth;
+        height = window.innerHeight;
+		frameWidth = Math.floor(width / numFrames) - 20;
+
+        window.addEventListener('resize', () => {
+            width = window.innerWidth;
+            height = window.innerHeight;
+			frameWidth = Math.floor(width / numFrames) - 20;
+        });
+    }
+
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<Greet />
+<!-- <Webview1 /> -->
+{#each inlineFrames as frame, i (frame)}
+	<iframe bind:this={frames[i]} id={frame} src={frame} height={height * 0.85} width={frameWidth} title={frame} />
+{/each}
+
+<textarea rows=4 cols=100 />
+<button>Send</button>
